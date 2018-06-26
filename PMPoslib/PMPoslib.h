c65ee17794a4362800dfd58f55c3a0af409d3e93
袁伟伟 <yuan_weiwei@suixingpay.com>
2018-06-26 14:49:27 +0800

    提交代码

--- PMPoslib/PMPoslib.h
+++ PMPoslib/PMPoslib.h
@@ -6,22 +6,6 @@
 //  Copyright © 2016年 Vbill Payment Co., Ltd. All rights reserved.
 //
 
-#import "BaseViewController.h"
-/** 信用卡列表页面 */
-#import "JGPopView.h"
-#import "BindCreditModel.h"
-@interface CreditCardListViewController : BaseViewController<selectIndexPathDelegate,UIAlertViewDelegate>
-
-@property(nonatomic,retain)UIImageView *errorShowView;
-@property(nonatomic,retain)UILabel *errorTitle;
-@property(nonatomic,retain)UILabel *errorString;
-
-/**交易失败进入绑定信用卡后，返回到刷卡的输入金额页面*/
-@property(nonatomic,copy)void(^gotoSwipeCardBlock)(BOOL gotoSwipeCard);
-
-#pragma mark 刷新数据
-- (void)CreditCardShouldReloadViewWithIndex:(BindCreditModel *)cellModel;
-#pragma mark 上传服务器删除某一条数据
-- (void)userCancellCreditCardWith:(BindCreditModel *)cellModel;
+@interface PMPoslib : NSObject
 
 @end
